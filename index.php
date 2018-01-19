27ad54df32e5c2c0c69b2a1926eb77e5c924b95e
jerin17 <jerinthomas17@gmail.com>
2018-01-19 18:05:02 +0530

    template

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>template</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+
+<div id="header">
+    <h1><a href="index.php">Hireling</a></h1>
+
+    <div id="navbar">
+      <ul>
+        <li><a href="#top">Home</a></li>         
+        <li><a href="#jobs">Jobs</a></li>         
+        <li><a href="#about">About Us</a></li>
+        <li><a href="#write">Write to us</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="#">Register</a></li>
+	    <li><a class="link" href="login.html">LOGIN</a></li>
+	  </ul>
+	 </div> 
+</div>
+	
+<section class="sec1">
+	
+</section>
+
+<section class="sec2">
+	<h1>Heading</h1>
+	<p>
+		Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+	</p>
+</section>
+<section class="sec3"></section>
+
+
+
+</body>
+</html>
