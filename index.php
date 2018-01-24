ac043efbc9b8975e9e4095ccdf2855194d2a160c
jerin17 <jerinthomas17@gmail.com>
2018-01-24 20:20:11 +0530

    about

--- index.php
+++ index.php
@@ -75,7 +75,7 @@
 	<h1>Got a Suggestion for us ?</h1>
 	<div class="contactbox">
 	<div class="cont-left">
-		<h3>Tell us how <br>you feel :))</h3>
+		<iframe height="100%" width="100%" style="border-radius: 10px 0px 0px 10px;" src="https://www.mapsdirections.info/en/custom-google-maps/map.php?width=420&height=420&hl=ru&q=Jamia%20Hamdard%2C%20Hamdard%20Nagar+(Hireling)&ie=UTF8&t=&z=15&iwloc=A&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.mapsdirections.info/en/custom-google-maps/">Create Google Map</a> by <a href="https://www.mapsdirections.info/en/">Measure area on map</a></iframe>
 	</div>
 	<form>
 	<div class="cont-right">
@@ -92,7 +92,64 @@
 
 </section>
 
+<section class="sec5">
+<div class="about">
+	<h1>Who are we </h1>
+	<p>Hireling.com is a freelancing and crowdsourcing marketplace by number of users and projects. We connect employers and freelancers globally from over the world. Through our marketplace, employers can hire freelancers to do work in areas such as software development, writing, data entry and design right through to engineering, the sciences, sales and marketing, accounting and legal services.</p>
 
+			<hr size="1" color="red" width="70%">
+<h2>Team</h2>
+</div>
+<div class="team">
+	<div class="teamcol" id="hide"></div>
+	<div class="teamcol">
+		<div class="teamimg">
+			<img src="css/about/josu.jpg" alt="josu jacob">
+		</div>
+		<div class="teamdetails">
+			<h1>Josu Jacob <br></h1><span>Jamia Hamdard University,<br> B.tech(CSE), final year</span>
+			<hr size="2" color="red" width="40%">
+			<ul>
+				<li><a href="https://www.facebook.com/josujacob"><i class="fa fa-facebook"></i></a></li>
+				<li><a href="https://plus.google.com/u/0/110015069409710534465"><i class="fa fa-google-plus"></i></a></li>
+				<li><a href="https://www.instagram.com/josu_jacob/"><i class="fa fa-instagram"></i></a></li>
+			</ul>
+		</div>
+	</div>
+
+	<div class="teamcol">
+		<div class="teamimg">
+			<img src="css/about/safin.jpg" alt="safin chowdhury">
+		</div>
+		<div class="teamdetails">
+			<h1>Safin Chowdhury <br></h1><span>Jamia Hamdard University,<br> B.tech(CSE), final year</span>
+			<hr size="2" color="red" width="40%">
+			<ul>
+				<li><a href="https://www.facebook.com/safin.chowdhury.35"><i class="fa fa-facebook"></i></a></li>
+				<li><a href="https://plus.google.com/u/0/112240593188290962758"><i class="fa fa-google-plus"></i></a></li>
+				<li><a href="https://www.instagram.com/chowdhury.safin/"><i class="fa fa-instagram"></i></a></li>
+			</ul>
+		</div>
+	</div>
+
+	<div class="teamcol">
+		<div class="teamimg">
+			<img src="css/about/jerin.jpg" alt="jerin thomas">
+		</div>
+		<div class="teamdetails">
+			<h1>Jerin Thomas <br></h1><span>Jamia Hamdard University,<br> B.tech(CSE), final year</span>
+			<hr size="2" color="red" width="40%">
+			<ul>
+				<li><a href="https://www.facebook.com/jerin.thomas.17"><i class="fa fa-facebook"></i></a></li>
+				<li><a href="https://plus.google.com/u/0/115607481997432307370"><i class="fa fa-google-plus"></i></a></li>
+				<li><a href="https://www.instagram.com/jerinthomas17/"><i class="fa fa-instagram"></i></a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="teamcol" id="hide"></div>
+
+</div>
+</section>
 
 </body>
 </html>
--- login.php
+++ login.php
@@ -13,14 +13,14 @@
 	
 <section class="sec4">
 	<div class="loginbox">
-	<a href="index.php"><i class="fa fa-close" style="position: absolute;top:5px;left:295px;color: red;font-size: 22px;"></i></a>
+	<a href="index.php"><i class="fa fa-close" style="position: absolute;top:-40px;left:265px;color: red;font-size: 22px;"></i></a>
 	<img src="css/user.png" class="avatar">	
 	<h1>Freelancer Login </h1>
 	<form>
 		<p>Email ID</p>
-		<input type="email" name="email" placeholder="Enter Email ID" required><i class="fa fa-envelope-o fa-fw" id="icon"></i>
+		<input type="email" name="email" placeholder="Enter Email ID" required><i class="fa fa-envelope-o" id="icon"></i>
 		<p>Password</p>
-		<input type="password" name="password" placeholder="Enter Password" required><i class="fa fa-key fa-fw" id="icon"></i>
+		<input type="password" name="password" placeholder="Enter Password" required><i class="fa fa-lock" id="icon"></i>
 		<input type="submit" value="Login" >
 		<a href="#">Forgot Password ?</a><br>
 		<a href="register.php">Don't have an account yet !</a>
